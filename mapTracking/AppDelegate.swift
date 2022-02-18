ef8e3e46a53911face571befdf4693b85b7087bc
omarmattr <omarabomattr@gmail.com>
2022-02-18 16:26:45 +0200

    in AppDelegate - config google map - config CLLocationManager to get location in assets - add car icon

--- mapTracking/AppDelegate.swift
+++ mapTracking/AppDelegate.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
+
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        GMSServices.provideAPIKey("AIzaSyAUltoacpwEf2mC5MHugX1dko4nymTMgUk")
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
         // Override point for customization after application launch.
         return true
     }
@@ -30,6 +40,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+
+
+}
+
+extension AppDelegate: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        print("ppppp \(location)")
+        guard let cls = locationUpdateCls else {
+            return
+        }
+        cls(location!)
+    }
+  
 
 
 }
